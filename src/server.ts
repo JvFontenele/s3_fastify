0a3eb6d99f64f6ebe7fd4e47c43cd660e2d04cde
Joaovnf <jvfont80@gmail.com>
2026-01-14 06:41:42 -0300

    adiciona arquivos de configuração e estrutura inicial do projeto

---
+++ src/server.ts
@@ -0,0 +1,47 @@
+import { fastify } from "fastify";
+import {
+  serializerCompiler,
+  validatorCompiler,
+  jsonSchemaTransform,
+  ZodTypeProvider,
+} from "fastify-type-provider-zod";
+
+import { fastifySwagger } from "@fastify/swagger";
+import { fastifyCors } from "@fastify/cors";
+import ScalarApiReference from "@scalar/fastify-api-reference";
+import { routes } from "@/routes";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(fastifyCors, {
+  origin: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  credentials: true,
+});
+
+
+
+app.register(fastifySwagger, {
+  openapi: {
+    info: {
+      title: "Api de distribuição de Arquivos",
+      description: "API de distribuição de arquivos para o S3",
+      version: "1.0.0",
+    },
+  },
+  transform: jsonSchemaTransform,
+});
+
+app.register(ScalarApiReference, {
+  routePrefix: "/docs",
+});
+
+app.register(routes);
+
+app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
+  console.log("HTTP Server Running on http://localhost:3333");
+  console.log("Docs Running on http://localhost:3333/docs");
+});
